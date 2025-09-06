3ba15dfdaf0c07a77441d7a08b77d2cca06022ab
FuensantaA <159921227+FuensantaA@users.noreply.github.com>
2025-09-06 15:11:44 +0200

    Add files via upload

---
+++ setuplab1.R
@@ -0,0 +1,8 @@
+# URL absolutas a los datos publicados en GitHub
+url_rdata <- "https://raw.githubusercontent.com/FuensantaA/para_av/refs/heads/main/datalab1.RData"
+url_csv   <- "https://raw.githubusercontent.com/FuensantaA/para_av/refs/heads/main/survey.csv"
+
+# Descarga silenciosa a la VFS de webR
+download.file(url_rdata, destfile = "datalab1.RData", mode = "wb")
+download.file(url_csv,   destfile = "survey.csv",     mode = "wb")
+
