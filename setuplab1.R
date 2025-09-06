e68aab21adcd2290ae618711b504568a90d14fd4
FuensantaA <159921227+FuensantaA@users.noreply.github.com>
2025-09-06 15:30:02 +0200

    Comment out setup URL download lines
    Commented out the setup URL download for setuplab1.R.

--- setuplab1.R
+++ setuplab1.R
@@ -1,11 +1,12 @@
 # URL absolutas a los datos publicados en GitHub
 url_rdata <- "https://raw.githubusercontent.com/FuensantaA/para_av/refs/heads/main/datalab1.RData"
 url_csv   <- "https://raw.githubusercontent.com/FuensantaA/para_av/refs/heads/main/survey.csv"
-url_setup   <- "https://raw.githubusercontent.com/FuensantaA/para_av/refs/heads/main/setuplab1.R"
+#url_setup   <- "https://raw.githubusercontent.com/FuensantaA/para_av/refs/heads/main/setuplab1.R"
 
 # Descarga silenciosa a la VFS de webR
 download.file(url_rdata, destfile = "datalab1.RData", mode = "wb")
 download.file(url_csv,   destfile = "survey.csv",     mode = "wb")
-download.file(url_setup,   destfile = "setuplab1.R",    mode = "wb")
+#download.file(url_setup,   destfile = "setuplab1.R",    mode = "wb")
+
 
 
